1b8aad49b2ad1b199061080e1af929c6be4ecf7a
kokmraoi <kokmraoi@gmail.com>
2022-04-29 15:08:04 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :lastname_kata, presence: true
+  validates :firstname_kata, presence: true
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :firstname
+    validates :lastname
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :firstname_kata
+    validates :lastname_kata
+  end
+  
+  
+
+  has_many :items
+  has_many :orders
 end

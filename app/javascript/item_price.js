5ffceb898b0f4dcb62c4779f0f9ff5ac435c807f
kokmraoi <kokmraoi@gmail.com>
2022-05-03 18:25:13 +0900

    商品出品機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener('input', () => {
+  const inputValue = priceInput.value;
+  
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+
+  const salesProfit = document.getElementById("profit")
+  const value_result = inputValue * 0.1
+  salesProfit.innerHTML = (inputValue - value_result);
+});
+});
---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :orders
+  
+  has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :date_of_shipment
+
+  validates :item_category_id, numericality: { other_than: 1, massage: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, massage: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, massage: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, massage: "can't be blank" }
+  validates :date_of_shipment_id, numericality: { other_than: 1, massage: "can't be blank" }
+
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :item_price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :image, presence: true
+
+end

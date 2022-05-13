3d0183d4700b2683599f206012ed40a24934ade0
kokmraoi <kokmraoi@gmail.com>
2022-05-13 20:41:29 +0900

    商品購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      pay_item
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
